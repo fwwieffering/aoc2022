aeed1ed24cf185902a18b9536331b273a83db8c9
Freddy Wieffering <fwieffering@gmail.com>
2022-12-13 12:31:20 -0600

    working part 1

--- internal/days/thirteen/thirteen.go
+++ internal/days/thirteen/thirteen.go
@@ -1,7 +1,10 @@
 package thirteen
 
 import (
+	"fmt"
 	"strconv"
+
+	puzzleinput "github.com/fwwieffering/aoc2022/internal/puzzle-input"
 )
 
 type Packet struct {
@@ -17,6 +20,80 @@ func (p *Packet) SetSide(side string, data []interface{}) {
 	}
 }
 
+// return two bools - valid, final. if valid and not final keep going
+func validate(left, right []interface{}, level int, debug bool) (bool, bool) {
+	buf := ""
+	for i := 0; i < level; i++ {
+		buf += "  "
+	}
+	if level == 0 && debug {
+		fmt.Printf("%s- Compare %+v vs %+v\n", buf, left, right)
+	}
+
+	valid := false
+	final := false
+
+	idx := 0
+	for idx < len(left) && idx < len(right) {
+		if debug {
+			fmt.Printf("%s  - Compare %d vs %d\n", buf, left[idx], right[idx])
+		}
+		leftInt, leftIsInt := left[idx].(int)
+		leftArr, leftIsArr := left[idx].([]interface{})
+		rightInt, rightIsInt := right[idx].(int)
+		rightArr, rightIsArr := right[idx].([]interface{})
+		if leftIsInt && rightIsInt {
+			// If the left integer is lower than the right integer, the inputs are in the right order
+			if leftInt != rightInt {
+				if leftInt < rightInt && debug {
+					fmt.Printf("%s    - Left side is smaller, so inputs are in the right order\n", buf)
+				} else if debug {
+					fmt.Printf("%s    - Right side is smaller, so inputs are *not* in the right order\n", buf)
+				}
+				return leftInt < rightInt, true
+			}
+		} else if rightIsArr || leftIsArr {
+			if !rightIsArr {
+				valid, final = validate(leftArr, []interface{}{rightInt}, level+1, debug)
+			} else if !leftIsArr {
+				valid, final = validate([]interface{}{leftInt}, rightArr, level+1, debug)
+			} else {
+				valid, final = validate(leftArr, rightArr, level+1, debug)
+			}
+			if final {
+				return valid, final
+			}
+		}
+		idx++
+	}
+	// If the left list runs out of items first, the inputs are in the right order
+	if len(left) != len(right) {
+		if len(left) < len(right) {
+			if debug {
+				fmt.Printf("%s    - Left side ran out of items, so inputs are in the right order\n", buf)
+			}
+			return true, true
+		} else {
+			if debug {
+				fmt.Printf("%s    - Right side ran out of items, so inputs are *not* in the right order\n", buf)
+			}
+			return false, true
+
+		}
+	}
+
+	return valid, final
+}
+
+func ValidatePackets(left []interface{}, right []interface{}, debug bool) bool {
+	valid, _ := validate(left, right, 0, debug)
+	return valid
+}
+func (p *Packet) Validate(debug bool) bool {
+	valid, _ := validate(p.Left, p.Right, 0, debug)
+	return valid
+}
+
 func parsePackets(in []byte) []*Packet {
 	final := []*Packet{}
 
@@ -24,6 +101,7 @@ func parsePackets(in []byte) []*Packet {
 	var curArr *[]interface{} = nil
 	parentArrays := []*[]interface{}{}
 	packetSide := "left"
+	intStr := ""
 
 	for idx, b := range in {
 		switch b {
@@ -47,23 +125,29 @@ func parsePackets(in []byte) []*Packet {
 				curArr = &newArr
 			}
 		case ']':
+			if intStr != "" {
+				i, _ := strconv.Atoi(string(intStr))
+				*curArr = append(*curArr, i)
+				intStr = ""
+			}
 			if len(parentArrays) > 0 {
 				parent := parentArrays[len(parentArrays)-1]
 				(*parent)[len(*parent)-1] = *curArr
 				curArr = parent
-
 				parentArrays = parentArrays[:len(parentArrays)-1]
 			} else {
 				currentPacket.SetSide(packetSide, *curArr)
 				curArr = nil
 			}
-		// don't care about commas - they are delimiters
 		case ',':
-			continue
+			if intStr != "" {
+				i, _ := strconv.Atoi(string(intStr))
+				*curArr = append(*curArr, i)
+				intStr = ""
+			}
 		// a number
 		default:
-			i, _ := strconv.Atoi(string(b))
-			*curArr = append(*curArr, i)
+			intStr += string(b)
 		}
 	}
 	// hanging packet
@@ -74,6 +158,23 @@ func parsePackets(in []byte) []*Packet {
 	return final
 }
 
+func part1(packets []*Packet) int {
+	part1 := 0
+	for idx, p := range packets {
+		if p.Validate(false) {
+			part1 += idx + 1
+		}
+	}
+	return part1
+}
+
 func Solve() error {
+	in := puzzleinput.Day(13)
+
+	packets := parsePackets(in)
+	fmt.Printf("Part1: %d\n", part1(packets))
+	// add divider packets, then sort for part 2
+	packets = append(packets)
+
 	return nil
 }
--- internal/days/thirteen/thirteen_test.go
+++ internal/days/thirteen/thirteen_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	puzzleinput "github.com/fwwieffering/aoc2022/internal/puzzle-input"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -67,12 +68,48 @@ var expected = []*Packet{
 	},
 }
 
+var validationExpectations = []bool{
+	true,
+	true,
+	false,
+	true,
+	false,
+	true,
+	false,
+	false,
+}
+
 func TestParsePackets(t *testing.T) {
 	result := parsePackets(example)
 	for idx, exp := range expected {
-		fmt.Printf("Left: %+v Right: %+v\n", result[idx].Left, result[idx].Right)
 		if !cmp.Equal(exp, result[idx]) {
-			t.Fatalf(cmp.Diff(exp, result[idx]))
+			fmt.Printf("left: %+v\nright:%+v\n", result[idx].Left, result[idx].Right)
+			t.Fatalf("idx: %d\n%s", idx, cmp.Diff(exp, result[idx]))
+		}
+	}
+
+	for idx, expectValid := range validationExpectations {
+		fmt.Printf("----------- %d -----------\n", idx)
+		isValid := result[idx].Validate(false)
+		if isValid != expectValid {
+			t.Fatalf("idx %d expected %t but got %t", idx, expectValid, isValid)
+		}
+	}
+	p1 := part1(result)
+	if p1 != 13 {
+		t.Fatalf("expected 13 got %d", p1)
+	}
+}
+
+var realResults = []bool{true, false, false, false, false, false, true, true, false, true, true, false, true, false, true, false, true, false, false, true, true, false, true, false, true, false, true, false, true, true, false, true, false, true, false, false, true, false, false, true, true, true, true, true, false, false, false, true, false, true, true, false, true, true, true, false, false, true, true, true, true, false, true, false, true, false, false, true, false, true, true, false, true, true, true, true, false, false, true, true, true, false, false, true, true, true, true, true, false, false, false, false, false, true, true, true, false, false, false, true, false, true, true, true, false, true, true, true, true, true, true, true, false, true, false, false, false, true, false, true, false, true, true, true, true, false, true, false, false, true, false, false, true, true, false, true, false, true, false, true, false, true, false, false, false, true, false, true, false, false}
+
+func TestRealResults(t *testing.T) {
+	pairs := parsePackets(puzzleinput.Day(13))
+	for idx, p := range pairs {
+		// fmt.Printf("----------- %d -----------\n", idx)
+		res := p.Validate(false)
+		if res != realResults[idx] {
+			t.Fatalf("error at idx %d. expected %t got %t", idx, realResults[idx], res)
 		}
 	}
 }
